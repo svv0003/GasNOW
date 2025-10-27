4691401ec2d2551e7590c0aae92b6bc020120327
svv0003 <ssetoo4689@naver.com>
2025-10-27 15:30:50 +0900

    Initial commit

---
+++ src/main/java/project/gasnow/GasNowApplication.java
@@ -0,0 +1,13 @@
+package project.gasnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GasNowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/gasnow/GasNowApplicationTests.java
@@ -0,0 +1,13 @@
+package project.gasnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasNowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
