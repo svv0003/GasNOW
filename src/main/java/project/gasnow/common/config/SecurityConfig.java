67298b553cb31f4fac0818eaebd944056c72b651
yunseonhyun <70725625+yunseonhyun@users.noreply.github.com>
2025-11-03 15:51:33 +0900

    gs_rating controller

--- src/main/java/project/gasnow/common/config/SecurityConfig.java
+++ src/main/java/project/gasnow/common/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -17,6 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated()
@@ -28,4 +34,5 @@ public class SecurityConfig {
 
         return http.build();
     }
+
 }
