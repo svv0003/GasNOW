863ed480e69c6a08b7dd975bfbc1e6fd3374857c
yunseonhyun <70725625+yunseonhyun@users.noreply.github.com>
2025-11-11 16:25:34 +0900

    프론트 백엔드 연결 및 프론트 디자인

--- src/main/java/project/gasnow/favorite/model/mapper/FavoriteMapper.java
+++ src/main/java/project/gasnow/favorite/model/mapper/FavoriteMapper.java
@@ -2,10 +2,14 @@ package project.gasnow.favorite.model.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import project.gasnow.favorite.model.dto.Favorite;
+import project.gasnow.rating.model.dto.Rating;
+
+import java.util.List;
 
 @Mapper
 public interface FavoriteMapper {
     Favorite getFavorite(String userId, String gsId);
+    List<String> getFavoriteList(String userId);
     void addFavorite(String userId, String gsId);
     void removeFavorite(String userId, String gsId);
 
--- src/main/java/project/gasnow/favorite/model/service/FavoriteService.java
+++ src/main/java/project/gasnow/favorite/model/service/FavoriteService.java
@@ -2,8 +2,11 @@ package project.gasnow.favorite.model.service;
 
 import project.gasnow.favorite.model.dto.Favorite;
 
+import java.util.List;
+
 public interface FavoriteService {
     String getFavorite(String userId, String gsId);
+    List<String> getFavoriteList(String userId);
     void addFavorite(Favorite favorite);
     void removeFavorite(Favorite favorite);
 }
