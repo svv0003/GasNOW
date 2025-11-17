dbbe3c607e9744331a980542fbcc586481e633f8
svv0003 <ssetoo4689@naver.com>
2025-11-17 11:47:20 +0900

    save3:

--- src/main/java/project/gasnow/common/config/WebConfig.java
+++ src/main/java/project/gasnow/common/config/WebConfig.java
@@ -9,38 +9,32 @@ import project.gasnow.common.interceptor.LoginInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-//
-//
-//    @Autowired
-//    private LoginInterceptor loginInterceptor;
-//
-//    @Autowired
-//    private AdminInterceptor adminInterceptor;
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//
-//        // 일반 회원이 로그인할 때 접속할 수 있는 API 리스트
-//        registry.addInterceptor(loginInterceptor)
-//                .addPathPatterns(       // 일반 회원이 추가로 접속할 수 있는 API 주소들
-//                        "/mypage"       // 마이 페이지
-//                )
-//                .excludePathPatterns(   // 누구든지 들어갈 수 있는 API 주소들
-//                        "/",            // 메인 페이지
-//                        "/map",         // 지도 페이지
-//                        "/login",       // 로그인 페이지
-//                        "/register",    // 회원가입 페이지
-//                        "/css/**",      // CSS 파일
-//                        "/js/**"        // JS 파일
-//                )
-//        ;
-//
-//        // 관리자 회원이 로그인할 때 추가로 접속할 수 있는 API 리스트
-//        registry.addInterceptor(adminInterceptor)
-//                .addPathPatterns(
-//
-//                )
-//        ;
-//    }
+
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Autowired
+    private AdminInterceptor adminInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        // 일반 회원이 로그인할 때 접속할 수 있는 API 리스트
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns(       // 일반 회원이 추가로 접속할 수 있는 API 주소들
+                        "/mypage",       // 마이 페이지
+                        "/favorites"
+                )
+                .excludePathPatterns(   // 누구든지 들어갈 수 있는 API 주소들
+                        "/",            // 메인 페이지
+                        "/map",         // 지도 페이지
+                        "/login",       // 로그인 페이지
+                        "/register",    // 회원가입 페이지
+                        "/css/**",      // CSS 파일
+                        "/js/**"        // JS 파일
+                )
+        ;
+    }
 
 }
